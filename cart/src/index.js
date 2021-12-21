86e39a2bfe15c9c9cdc5a988902a2982fbe5a9fb
Bhupen <bhupen@Bhupens-MacBook-Pro.local>
2021-12-21 14:34:44 -0800

    updated msg

--- cart/src/index.js
+++ cart/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/cart', (req, res) => {
-    res.json({ message: 'Once again hello from cart service' })
+    res.json({ message: 'Once again hello from CART Service again' })
 })
 
 app.listen(3001, () => console.log('listening on port 3001'));
